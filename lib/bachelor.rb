8d67ef89af1732b7557c33d54d01a665c66163ed
Mike Luo <zluo@alumni.berklee.edu>
2017-04-19 01:45:16 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,54 @@
+require "pry"
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  season = data[season]
+  i = 0
+  while i < season.length
+    if season[i]["status"] == "Winner"
+      return season[i]["name"].split(" ")[0]
+    else
+      i += 1
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      if contestant["occupation"] == occupation
+        return contestant["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  town_count = 0
+
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      if contestant["hometown"] == hometown
+        town_count += 1
+      end
+    end
+  end
+  town_count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      if contestant["hometown"] == hometown
+        return contestant["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  season = data[season]
+  added_age = 0
+  season.each{ |contestant| added_age += contestant["age"].to_f }
+  average = added_age / season.length
+  average.round
 end
